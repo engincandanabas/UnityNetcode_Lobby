8ab1ed850f72cc26299fa4a54c09bcbd7b1c2e63
Engincan Danabaş <60680749+engincandanabas@users.noreply.github.com>
2025-04-21 16:08:07 +0300

    Auth

---
+++ Assets/Scripts/AuthenticateUI.cs
@@ -0,0 +1,49 @@
+using TMPro;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AuthenticateUI : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField usernameField;
+    [SerializeField] private Button loginButton;
+
+    public static string playerName="";
+    private void Awake()
+    {
+        loginButton.onClick.AddListener(async () =>
+        {
+            playerName = usernameField.text;
+            InitializationOptions initializationOptions = new InitializationOptions();
+            initializationOptions.SetProfile(playerName);
+
+            await UnityServices.InitializeAsync(initializationOptions);
+
+            AuthenticationService.Instance.SignedIn += () => {
+                // do nothing
+                Debug.Log("Signed in! " + AuthenticationService.Instance.PlayerId);
+
+                
+            };
+
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        });
+
+        usernameField.onValueChanged.AddListener(Usernamefield_OnValueChanged);
+    }
+
+    private void Usernamefield_OnValueChanged(string value)
+    {
+        if (value.Length > 2)
+            loginButton.interactable = true;
+        else
+            loginButton.interactable = false;
+    }
+    private void Hide()
+    {
+        usernameField.gameObject.SetActive(false);
+        loginButton.gameObject.SetActive(false);    
+    }
+}
