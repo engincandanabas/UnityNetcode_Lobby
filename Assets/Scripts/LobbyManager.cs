fbee4f29ed307520f00da7163c181c96fe484dce
Engincan Danabaş <60680749+engincandanabas@users.noreply.github.com>
2025-04-23 13:41:45 +0300

    Relay Manager

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -9,10 +9,14 @@ using Unity.Services.Lobbies.Models;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Runtime.CompilerServices;
 
 public class LobbyManager : MonoBehaviour
 {
     public static LobbyManager Instance { get; private set; }
+
+    public static bool IsHost { get; private set; }
+    public static string RelayJoinCode { get; private set; }
 
     public const string KEY_PLAYER_NAME = "PlayerName";
     public const string KEY_GAME_MODE = "GameMode";
@@ -40,8 +44,11 @@ public class LobbyManager : MonoBehaviour
 
 
     private float heartbeatTimer;
+    private float lobbyPollTimer;
+    private float refreshLobbyListTimer = 2f;
     private string playerName = "";
     private Lobby joinedLobby;
+    private bool alreadyStartedGame;
 
     private void Awake()
     {
@@ -50,6 +57,7 @@ public class LobbyManager : MonoBehaviour
     private void Update()
     {
         HandleLobbyHeartbeat();
+        HandleLobbyPolling();
     }
     private async void HandleLobbyHeartbeat()
     {
@@ -63,6 +71,53 @@ public class LobbyManager : MonoBehaviour
 
                 Debug.Log("Heartbeat");
                 await LobbyService.Instance.SendHeartbeatPingAsync(joinedLobby.Id);
+            }
+        }
+    }
+    private async void HandleLobbyPolling()
+    {
+        if (joinedLobby != null)
+        {
+            lobbyPollTimer -= Time.deltaTime;
+            if (lobbyPollTimer < 0f)
+            {
+                float lobbyPollTimerMax = 1.1f;
+                lobbyPollTimer = lobbyPollTimerMax;
+
+                joinedLobby = await LobbyService.Instance.GetLobbyAsync(joinedLobby.Id);
+
+                OnJoinedLobbyUpdate?.Invoke(this, new LobbyEventArgs { lobby = joinedLobby });
+
+                if (!IsLobbyHost())
+                {
+                    if (joinedLobby.Data[KEY_RELAY_JOIN_CODE].Value != "")
+                    {
+                        JoinGame(joinedLobby.Data[KEY_RELAY_JOIN_CODE].Value);
+                    }
+                }
+
+                if (!alreadyStartedGame)
+                {
+                    if (IsLobbyHost())
+                    {
+                        if (joinedLobby.Players.Count == 2)
+                        {
+                            // Two players have joined, start game
+                            StartGame();
+                        }
+                    }
+                }
+
+
+                if (!IsPlayerInLobby())
+                {
+                    // Player was kicked out of this lobby
+                    Debug.Log("Kicked from Lobby!");
+
+                    OnKickedFromLobby?.Invoke(this, new LobbyEventArgs { lobby = joinedLobby });
+
+                    joinedLobby = null;
+                }
             }
         }
     }
@@ -129,6 +184,21 @@ public class LobbyManager : MonoBehaviour
             }
         }
     }
+    private bool IsPlayerInLobby()
+    {
+        if (joinedLobby != null && joinedLobby.Players != null)
+        {
+            foreach (Player player in joinedLobby.Players)
+            {
+                if (player.Id == AuthenticationService.Instance.PlayerId)
+                {
+                    // This player is in this lobby
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     public async void RefreshLobbyList()
     {
         try
@@ -154,6 +224,47 @@ public class LobbyManager : MonoBehaviour
             QueryResponse lobbyListQueryResponse = await LobbyService.Instance.QueryLobbiesAsync();
 
             OnLobbyListChanged?.Invoke(this, new OnLobbyListChangedEventArgs { lobbyList = lobbyListQueryResponse.Results });
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+    private void JoinGame(string relayJoinCode)
+    {
+        Debug.Log("JoinGame " + relayJoinCode);
+        if (string.IsNullOrEmpty(relayJoinCode))
+        {
+            Debug.Log("Invalid Relay code, wait");
+            return;
+        }
+
+        IsHost = false;
+        RelayJoinCode = relayJoinCode;
+        SceneManager.LoadScene(1);
+        alreadyStartedGame = true;
+        OnLobbyStartGame?.Invoke(this, new LobbyEventArgs { lobby = joinedLobby });
+    }
+    public async void StartGame()
+    {
+        try
+        {
+            Debug.Log("StartGame");
+
+            Lobby lobby = await LobbyService.Instance.UpdateLobbyAsync(joinedLobby.Id, new UpdateLobbyOptions
+            {
+                Data = new Dictionary<string, DataObject> {
+                    { KEY_START_GAME, new DataObject(DataObject.VisibilityOptions.Public, "1") }
+                }
+            });
+
+            joinedLobby = lobby;
+
+            IsHost = true;
+            alreadyStartedGame = true;
+            SceneManager.LoadScene(1);
+
+            OnLobbyStartGame?.Invoke(this, new LobbyEventArgs { lobby = joinedLobby });
         }
         catch (LobbyServiceException e)
         {
